cedd5e8ea07103e25c53d3bfd6d384b824c3fed1
Oleksandr Moshta <ahonymous@gmail.com>
2023-08-25 11:47:11 +0300

    feat: module3 - slices (#2)

--- main.go
+++ main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"coursera/pkg/module1"
 	"coursera/pkg/module2"
+	"coursera/pkg/module3"
 	"fmt"
 )
 
 var moduleNumber int
-var modules = [4]string{
+var modules = [5]string{
 	"1. Module 1: Hello, world!!!",
 	"2. Module 2: Truncation enters a floating point number and prints the integer, truncated version of the floating point number that was entered.",
 	"3. Module 2: Findian reads a string from the user, prints whether or not the string contains the characters a, i, and n, in order, with any number of characters between the three letters.",
+	"4. Module 3: Slice reads integers from the user until the user enters the character X instead of an integer. Then prints the integers in the reverse order that they were read.",
 	"0. Exit",
 }
 
 func main() {
 	for {
-		fmt.Println("What homework are you want to check?")
+		fmt.Println("What homework are you want to check? (any not numeric symbol to exit)")
 		fmt.Println("========================================")
 
 		for _, module := range modules {
@@ -24,8 +26,8 @@ func main() {
 		}
 		_, err := fmt.Scan(&moduleNumber)
 
-		if err != nil {
-			fmt.Println(err)
+		if nil != err || 0 == moduleNumber {
+			fmt.Println("Exit...")
 
 			return
 		}
@@ -37,11 +39,8 @@ func main() {
 			module2.Trunc()
 		case 3:
 			module2.Fundian()
-		case 0:
-			fmt.Println("Bye!")
-			return
-		default:
-			fmt.Println("Wrong module number! Try again.")
+		case 4:
+			module3.Slice()
 		}
 
 		fmt.Println("----------------------------------------")
