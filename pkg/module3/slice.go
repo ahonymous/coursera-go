c9feed82d46000a8ecd6fcae30f676d0b06bbfee
Oleksandr Moshta <ahonymous@gmail.com>
2023-08-25 11:17:41 +0300

    feat: module3 - slices

---
+++ pkg/module3/slice.go
@@ -0,0 +1,42 @@
+package module3
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func Slice() {
+	intSlice := make([]int, 3)
+
+	for {
+		fmt.Print("Enter an integer (X to quit): ")
+		var input string
+		_, err := fmt.Scanln(&input)
+
+		if err != nil {
+			fmt.Println("Something went wrong:", err)
+
+			break
+		}
+
+		input = strings.ToLower(input)
+		if "x" == input {
+			fmt.Println("Exit...")
+
+			break
+		}
+
+		number, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Please, enter a valid integer number and try again.")
+
+			continue
+		}
+
+		intSlice = append(intSlice, number)
+		sort.Ints(intSlice)
+		fmt.Println("The sorted slice is:", intSlice)
+	}
+}
