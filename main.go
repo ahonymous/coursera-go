41c65d0bb52eb994522343d820704e3a753d7edb
Oleksandr Moshta <ahonymous@gmail.com>
2023-08-27 10:20:23 +0300

    feat: module4 - make json

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 	"coursera/pkg/module1"
 	"coursera/pkg/module2"
 	"coursera/pkg/module3"
+	"coursera/pkg/module4"
 	"fmt"
 )
 
 var moduleNumber int
-var modules = [5]string{
+var modules = [6]string{
 	"1. Module 1: Hello, world!!!",
 	"2. Module 2: Truncation enters a floating point number and prints the integer, truncated version of the floating point number that was entered.",
 	"3. Module 2: Findian reads a string from the user, prints whether or not the string contains the characters a, i, and n, in order, with any number of characters between the three letters.",
 	"4. Module 3: Slice reads integers from the user until the user enters the character X instead of an integer. Then prints the integers in the reverse order that they were read.",
+	"5. Module 4: Makejson reads a name and an address from the user, stores them in a map, and then converts the map to JSON and prints it.",
 	"0. Exit",
 }
 
@@ -41,6 +43,8 @@ func main() {
 			module2.Fundian()
 		case 4:
 			module3.Slice()
+		case 5:
+			module4.MakeJson()
 		}
 
 		fmt.Println("----------------------------------------")
