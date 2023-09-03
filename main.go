d3c2e8ea9edf5e1c2de88ca808b885402040cf49
Oleksandr Moshta <ahonymous@gmail.com>
2023-09-04 01:00:03 +0300

    feat: bubble sort

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import course1 "coursera/pkg/course1StartingWithGo"
+import (
+	"coursera/pkg/course1StartingWithGo"
+	"coursera/pkg/course2FunctionsMethodsInterfaceswithGo"
+	"fmt"
+)
+
+var courseNumber int
+var courses = [3]string{
+	"1. Course 1: Starting with Go",
+	"2. Course 2: Functions, Methods, and Interfaces in Go",
+	"0. Exit",
+}
 
 func main() {
-	course1.Start()
+	for {
+		fmt.Println("What course are you want to check? (any not numeric symbol to exit)")
+		fmt.Println("========================================")
+
+		for _, course := range courses {
+			fmt.Println(course)
+		}
+		_, err := fmt.Scan(&courseNumber)
+
+		if nil != err || 0 == courseNumber {
+			fmt.Println("Exit...")
+
+			return
+		}
+
+		switch courseNumber {
+		case 1:
+			course1StartingWithGo.Start()
+		case 2:
+			course2FunctionsMethodsInterfaceswithGo.Start()
+		}
+
+		fmt.Println("========================================")
+	}
 }
